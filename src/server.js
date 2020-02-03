44da67561926cea22d6fde069528108b5f289a47
Yannick Gbaguidi <gyannick97@gmail.com>
2020-02-03 22:58:59 +0100

    Update of server.js

--- src/server.js
+++ src/server.js
@@ -1,21 +1,16 @@
-const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
-const port = process.env.PORT || 8080;
-const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
+const express = require('express')
+const path = require('path')
+const app = express()
 
-// the __dirname is the current directory from where the script is running
-
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.listen(port);
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  // Handle React routing, return all requests to React app
+  app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  })
+}
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+  console.log(`API listening on port ${port}...`)
+})
